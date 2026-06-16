6e69364cc9fb6aaf32b5c6838fe465a57bc79835
VijDavid <150273326+VijDavid@users.noreply.github.com>
2026-06-16 23:38:51 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { nitro } from "nitropack/vite";
 
 export default defineConfig({
   vite: {
-   plugins: [react()]
+    plugins: [nitro()],
   },
   tanstackStart: {
     server: { entry: "server" },
